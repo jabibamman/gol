bcfe7e608ce0195768a0d2f1050312d84190f6aa
James ABIB <james.bidule@gmail.com>
2024-07-18 23:01:02 +0200

    fix(steps): Enhanced GameOfLife test suite
    Expanded the testing capabilities for the GameOfLife simulation. Added new methods to handle grid initialization, graphical representation of cells and borders, and cell position tracking. Improved existing methods for grid updates and cell status checks.

--- src/test/kotlin/GameOfLifeSteps.kt
+++ src/test/kotlin/GameOfLifeSteps.kt
@@ -10,22 +10,69 @@ class GameOfLifeSteps {
 
     private lateinit var grid: Grid
 
+    @Given("a 3x3 grid of dead cells")
+    fun a3x3GridOfDeadCells() {
+        grid = Grid(3, 3)
+    }
+
+    @Given("a coordinate positions system represented by the syntax {string}")
+    fun aCoordinatePositionsSystem(syntax: String) { }
+
+    @Given("the top left cell is at position [{int},{int}]")
+    fun theTopLeftCellIsAtPosition(x: Int, y: Int) { }
+    @Given("the bottom left cell is at position [{int},{int}]")
+    fun theBottomLeftCellIsAtPosition(x: Int, y: Int) { }
+
+    @Given("a live cell is graphically represented by the ASCII character {string}")
+    fun aLiveCellIsGraphicallyRepresentedBy(character: String) { }
+
+    @Given("a dead cell is graphically represented by the ASCII character {string}")
+    fun aDeadCellIsGraphicallyRepresentedBy(character: String) { }
+
+    @Given("borders are graphically represented by the ASCII character {string}")
+    fun bordersAreGraphicallyRepresentedBy(character: String) { }
+
+    @Then("the graphical representation should be")
+    fun theGraphicalRepresentationShouldBe(dataTable: DataTable) {
+        val expectedRepresentation = dataTable.asLists().map { it.joinToString("|", "|", "|") }
+        val actualRepresentation = grid.toString().split("\n")
+        assertEquals(expectedRepresentation, actualRepresentation)
+    }
+
     @Given("the following {int}x{int} grid")
     fun theFollowingGrid(rows: Int, cols: Int, dataTable: DataTable) {
-        grid = Array(rows) { Array(cols) { false } }
+        grid = Grid(rows, cols)
         val data = dataTable.asLists(String::class.java)
         for (i in data.indices) {
             for (j in data[i].indices) {
-                grid[i][j] = data[i][j] == "X"
+                if (data[i][j] == "X") {
+                    grid.setAsAlive(i, j)
+                } else {
+                    grid.setAsDead(i, j)
+                }
             }
         }
     }
 
     @When("I update the grid")
     fun iUpdateTheGrid() {
-        grid = Grid(3, 3)
-
         GameOfLife(grid).update()
+    }
+
+    @Then("the grid should be")
+    fun theGridShouldBe(expectedDataTable: DataTable) {
+        val expectedGrid = Grid(grid.getLimits().x + 1, grid.getLimits().y + 1)
+        val data = expectedDataTable.asLists(String::class.java)
+        for (i in data.indices) {
+            for (j in data[i].indices) {
+                if (data[i][j] == "X") {
+                    expectedGrid.setAsAlive(i, j)
+                } else {
+                    expectedGrid.setAsDead(i, j)
+                }
+            }
+        }
+        assertEquals(expectedGrid.toString(), grid.toString())
     }
 
     @Then("the cell at [{int},{int}] should be alive")
@@ -38,15 +85,8 @@ class GameOfLifeSteps {
         assertEquals(false, grid.isDead(row, col))
     }
 
-    @Then("the grid should be")
-    fun theGridShouldBe(expectedDataTable: DataTable) {
-        val expectedGrid = Array(grid.size) { Array(grid[0].size) { false } }
-        val data = expectedDataTable.asLists(String::class.java)
-        for (i in data.indices) {
-            for (j in data[i].indices) {
-                expectedGrid[i][j] = data[i][j] == "X"
-            }
-        }
-        assertEquals(expectedGrid, grid)
+    @Then("the cell at position [{int},{int}] stays alive")
+    fun theCellAtPositionStaysAlive(row: Int, col: Int) {
+        assertEquals(true, grid.isAlive(row, col))
     }
 }

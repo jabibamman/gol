03b14fafc696347e6db1ea58eeb97ad35bb40a16
MarcMalha <marc.malha@beanstock.com>
2024-07-18 19:11:34 +0200

    Chore: refactor code to abide by new class

--- src/test/kotlin/GameOfLifeSteps.kt
+++ src/test/kotlin/GameOfLifeSteps.kt
@@ -7,35 +7,37 @@ import kotlin.test.assertEquals
 
 class GameOfLifeSteps {
 
-    private lateinit var grid: Array<Array<Boolean>>
+    private lateinit var grid: Grid
 
     @Given("a {int}x{int} grid")
     fun aGrid(rows: Int, cols: Int) {
-        grid = Array(rows) { Array(cols) { false } }
+        grid = Grid(rows, cols)
     }
 
     @Given("a live cell at [{int}, {int}]")
     fun aLiveCellAt(row: Int, col: Int) {
-        grid[row][col] = true
+        grid.setAsAlive(row, col)
     }
 
     @Given("a dead cell at [{int}, {int}]")
     fun aDeadCellAt(row: Int, col: Int) {
-        grid[row][col] = false
+        grid.setAsDead(row, col)
     }
 
     @When("I update the grid")
     fun iUpdateTheGrid() {
-        grid = GameOfLife().update(grid)
+        grid = Grid(3, 3)
+
+        GameOfLife(grid).update()
     }
 
     @Then("the cell at [{int}, {int}] should be alive")
     fun theCellAtShouldBeAlive(row: Int, col: Int) {
-        assertEquals(true, grid[row][col])
+        assertEquals(true, grid.isAlive(row, col))
     }
 
     @Then("the cell at [{int}, {int}] should be dead")
     fun theCellAtShouldBeDead(row: Int, col: Int) {
-        assertEquals(false, grid[row][col])
+        assertEquals(false, grid.isDead(row, col))
     }
 }
